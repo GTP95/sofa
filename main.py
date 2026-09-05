54ea7e4d7947cbcca8b210a50ead40925380c531
moneropool <notme@example.com>
2026-09-05 18:53:30 +0200

    Bundled Keccak simulation working

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     elif args.target == "KECCAK":
         ig = KeccakHashInputsGenerator()
         iv = KeccakHashInputValidator()
-        sl = KeccakHashSettingsLoader()
+        sl = KeccakHashSettingsLoader(json_path=args.config)
         ip = KeccakHashInputParser()
         tp = KeccakHashQilingProfile()
 
--- sofa/utils/helpers.py
+++ sofa/utils/helpers.py
@@ -630,8 +630,7 @@ def initialize_qiling(
             logging.getLogger().warning('This part of the codebase needs refactoring and will not work.')
             config = AsconSettingsLoader().get_target_settings()
         case 'KECCAK':
-            logging.getLogger().warning('This part of the codebase needs refactoring and will not work.')
-            config = KeccakHashSettingsLoader().get_target_settings()
+            config = KeccakHashSettingsLoader(json_path).get_target_settings()
         case _:
             raise ValueError(f"Algorithm {profile.get_algorithm_name()} not supported.")
 
